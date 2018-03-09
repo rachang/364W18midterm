e66e44610213c3014370464695a5525caa574ed6
rachang <rachang@umich.edu>
2018-03-09 00:36:29 -0500

    Third Commit

--- SI364midterm.py
+++ SI364midterm.py
@@ -7,9 +7,12 @@
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField # Note that you may need to import more here! Check out examples that do what you want to figure out what.
-from wtforms.validators import Required # Here, too
+from wtforms import StringField, SubmitField, ValidationError # Note that you may need to import more here! Check out examples that do what you want to figure out what.
+from wtforms.validators import Required, Length # Here, too
 from flask_sqlalchemy import SQLAlchemy
+#import statements for api
+import urllib.request
+import json
 
 ## App setup code
 app = Flask(__name__)
@@ -17,7 +20,11 @@ app.debug = True
 app.use_reloader = True
 
 ## All app.config values
-
+app.config['SECRET_KEY'] = 'hard to guess string from si364'
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:morningstar@localhost/SI364midterm"
+## Provided:
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 ## Statements for db setup (and manager setup if using Manager)
 db = SQLAlchemy(app)
@@ -58,12 +65,13 @@ class NameForm(FlaskForm):
 ###### VIEW FXNS ######
 #######################
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def home():
     form = NameForm() # User should be able to enter name after name and each one will be saved, even if it's a duplicate! Sends data with GET
     if form.validate_on_submit():
+        print("hello")
         name = form.name.data
-        newname = Name(name)
+        newname = Name(name=name)
         db.session.add(newname)
         db.session.commit()
         return redirect(url_for('all_names'))
@@ -83,3 +91,6 @@ def all_names():
 
 # Put the code to do so here!
 # NOTE: Make sure you include the code you need to initialize the database structure when you run the application!
+if __name__ == '__main__':
+    db.create_all() # Will create any defined models when you run the application
+    app.run(use_reloader=True,debug=True) # The usual
